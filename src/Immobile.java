58281f957257f375836cd39cb60512a283ed7016
leoroca98 <leoroca12345@gmail.com>
2023-08-01 11:34:11 +0200

    inizio esercizio

---
+++ src/Immobile.java
@@ -0,0 +1,25 @@
+public class Immobile {
+    protected String codice;
+    protected String indirizzo;
+    protected String cap;
+
+    protected String citta;
+    protected int superficie;
+    protected int personeInteressate;
+
+    public Immobile(String codice, String indirizzo, String cap, String citta, int superficie) {
+        this.codice = codice;
+        this.indirizzo = indirizzo;
+        this.cap = cap;
+        this.citta = citta;
+        this.superficie = superficie;
+        this.personeInteressate = 0;
+
+    }
+
+
+}
+
+
+
+
